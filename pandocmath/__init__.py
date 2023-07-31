14f2e5ff40e2c1a802b1fcae7431a06eb5da7bf4
Gavin McWhinnie <s2091628@ed.ac.uk>
2023-07-31 14:49:14 +0100

    Added command-line arguments

--- pandocmath/__init__.py
+++ pandocmath/__init__.py
@@ -25,11 +25,15 @@ SOFTWARE.
 import logging
 import sys
 import panflute as pf
+import argparse
+from pathlib import Path
 
 from pandocmath.ams import AmsthmSettings, AmsTheorem, amsthm_numbering, resolve_ref
 
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+OUTPUT_FORMATS = pf.run_pandoc(args=['--list-output-formats']).split('\r\n')
 
 MATHJAX_CONFIG = "<script> MathJax = {   loader: {     load: [\'[custom]/xypic.js\'],     paths: {custom: \'https://cdn.jsdelivr.net/gh/sonoisa/XyJax-v3@3.0.1/build/\'}   },   tex: {     packages: {\'[+]\': [\'xypic\']},     macros : { relax: ''},     tags: 'ams'   } }; </script>\n\n"
 
@@ -51,13 +55,39 @@ def finalize(doc : pf.Doc) -> None:
     doc.metadata.content['header-includes'] = pf.MetaBlocks(raw_HEADER)
     del doc._amsthm_settings
 
-def main(doc: pf.Doc | None = None) -> None:
-    return pf.run_filters(
-        (action1, action2),
-        prepare=prepare,
-        finalize=finalize,
-        doc=doc,
-    )
+def main() -> None:
+    parser = argparse.ArgumentParser( description='Hello world!')
+    parser.add_argument('file', help='TeX file to be converted')
+    #parser.add_argument('-o', default='output.html', help='output file')
+    args = parser.parse_args()
+
+    if args.file in OUTPUT_FORMATS:
+        #### The filter is being called by Pandoc
+        target_format : str = args.file
+        if target_format == 'html':
+            doc = pf.load()
+            pf.run_filters(
+                (action1, action2),
+                prepare=prepare,
+                finalize=finalize,
+                doc=doc,
+            )
+            pf.dump(doc)
+        else:
+            logger.error('The filter pandoc-math is only intended for converting with output to html.')
+    else:
+
+        path = Path(args.file)
+        if path.is_file():
+            if (filetype := path.suffix.lower()) == '.tex':
+                ####### TODO: finish here
+                pass
+            else:
+                logger.error("Please input a .tex file only.")
+        else:
+            logger.error("No such file exists: "+str(args.file)+"\nPlease try again.")
+
+
 
 if __name__ == "__main__":
     main()
