a2fe3f762de6cf7a7c949ef159bfb4dbb9071cea
Gavin McWhinnie <s2091628@ed.ac.uk>
2023-08-09 15:54:11 +0100

    Testing Pandoc Tests

--- tests/tests.py
+++ tests/tests.py
@@ -2,12 +2,7 @@ import panflute as pf
 
 text = "\\begin{document}Hello World\\end{document}"
 
-res = pf.convert_text(text, input_format='latex', output_format='html', standalone=False, extra_args=None, pandoc_path=None)
-
-# content of test_sample.py
-def inc(x):
-    return x + 1
-
-
-def test_answer():
-    assert inc(3) == 4
+def test_pandoc():
+    res = pf.convert_text(text, input_format='latex', output_format='html', standalone=False, extra_args=None, pandoc_path=None)
+    ans = "<p>Hello World</p>"
+    assert res == ans
